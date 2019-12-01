f21e9d043ec77c75d1d0e2a7198df8dbfcd2bdce
Ibtis0001 <ebt.0101@gmail.com>
2019-12-01 15:35:27 +0300

    p3-4

--- mypro 7/index.js
+++ mypro 7/index.js
@@ -2,29 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const Home = require('./roots/Home')
+const app = express();
 
 //must change your port to this for deployment else it wont work
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000
 
 //serves all our static files from the build directory.
 app.use(express.static(path.join(__dirname, "build")));
 
 // After all routes
 // This code essentially serves the index.html file on any unknown routes.
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
-app.listen(PORT);
 
-const Home = require('./roots/Home')
-const app = express();
 app.use(cors())
 // import route
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/',Home);
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 
 
@@ -35,6 +33,5 @@ mongoose.connect('mongodb+srv://moe:Aa7788000@moe-pxfnp.gcp.mongodb.net/test?ret
 })
 
 
-const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>console.log(`app is up on PORT ${PORT}`));
